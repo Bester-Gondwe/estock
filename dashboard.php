f27207c3b7be9ff801fa6768624d884a0d5b0ee8
Bester Gondwe <143722159+Bester-Gondwe@users.noreply.github.com>
2025-01-05 09:31:17 +0200

    session

--- dashboard.php
+++ dashboard.php
@@ -38,7 +38,7 @@ try {
 
 // Redirect to login if the user is not logged in
 if (!isset($_SESSION['user_id'])) {
-  header("Location: login.php");
+  header("Location: dashboard.php");
   exit;
 }
 ?>
