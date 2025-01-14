d55dbbecc2404c8b87e234f5a951bce89cfdb6af
caesar <ckalikude@gmail.com>
2025-01-14 23:52:01 +0200

    initial commit

--- initialize.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!defined('base_url')) define('base_url', 'http://localhost/WebProgrammingClass/estock/');
-if (!defined('SYSTEM_NAME')) define('SYSTEM_NAME', 'eStock');
-if (!defined('DB_SERVER')) define('DB_SERVER', "localhost");
-if (!defined('DB_USERNAME')) define('DB_USERNAME', "root");
-if (!defined('DB_PASSWORD')) define('DB_PASSWORD', null);
-if (!defined('DB_NAME')) define('DB_NAME', "estock");
