d92f16d81cec7abbb86989f7f2aae73234c4210c
caesar <ckalikude@gmail.com>
2025-01-16 13:20:02 +0200

    initailhhjd

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ session_start();
     <div class="landing-page">
       <h1>Welcome to Our Store</h1>
       <p>Discover amazing products and services designed for you.</p>
-      <a href="#" class="cta-button">Shop Now</a>
+      <a href="category.php?category=Phones" class="cta-button">Shop Now</a>
     </div>
 
   </div>
