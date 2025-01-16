5b94d44bb18c193f29fc1e8290e2958614dbac85
caesar <ckalikude@gmail.com>
2025-01-16 14:38:30 +0200

    sklalks

--- sql/estock_file.sql
+++ sql/estock_file.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 13, 2025 at 01:53 PM
+-- Generation Time: Jan 16, 2025 at 01:37 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -32,13 +32,6 @@ CREATE TABLE `categories` (
   `category_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `categories`
---
-
-INSERT INTO `categories` (`category_id`, `category_name`) VALUES
-(1, 'Phones');
-
 -- --------------------------------------------------------
 
 --
@@ -49,21 +42,9 @@ CREATE TABLE `orders` (
   `order_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `order_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `order_status` varchar(10) NOT NULL
+  `order_status` varchar(10) NOT NULL,
+  `amount` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `orders`
---
-
-INSERT INTO `orders` (`order_id`, `user_id`, `order_date`, `order_status`) VALUES
-(1, 13, '2025-01-13 11:00:27', 'Cancled'),
-(2, 13, '2025-01-12 19:50:51', 'Pending'),
-(3, 13, '2025-01-12 19:50:51', 'Pending'),
-(4, 14, '2025-01-13 10:51:37', 'Cancled'),
-(5, 15, '2025-01-13 09:32:44', 'Derivered'),
-(6, 16, '2025-01-13 10:51:02', 'Derivered'),
-(7, 14, '2025-01-12 19:50:51', 'Pending');
 
 -- --------------------------------------------------------
 
@@ -76,20 +57,6 @@ CREATE TABLE `order_details` (
   `product_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `order_details`
---
-
-INSERT INTO `order_details` (`order_id`, `product_id`, `quantity`) VALUES
-(1, 58, 2),
-(2, 54, 1),
-(1, 59, 5),
-(7, 59, 4),
-(7, 59, 4),
-(6, 59, 4),
-(5, 59, 4),
-(4, 59, 4);
 
 -- --------------------------------------------------------
 
@@ -108,15 +75,6 @@ CREATE TABLE `products` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `products`
---
-
-INSERT INTO `products` (`product_id`, `product_name`, `product_description`, `product_price`, `category_id`, `quantity`, `user_id`, `created_at`) VALUES
-(54, 'Dell 3567', 'New Laptop', 30000, 1, 30, 11, '2025-01-12 11:25:08'),
-(58, 'Redmi', 'phone', 10000, 1, 7, 12, '2025-01-10 04:56:57'),
-(59, 'TT-Shirt', 'Brand new clothes', 3000, 1, 30, 11, '2025-01-13 07:42:34');
-
 -- --------------------------------------------------------
 
 --
@@ -129,16 +87,6 @@ CREATE TABLE `product_images` (
   `is_primary` tinyint(1) NOT NULL,
   `product_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `product_images`
---
-
-INSERT INTO `product_images` (`image_id`, `file_name`, `is_primary`, `product_id`) VALUES
-(175, '67803e9052090-114.jpg', 0, 54),
-(183, '6780a899586bd-117.jpg', 1, 58),
-(184, '6780effed5702-130.jpg', 0, 54),
-(185, '6783a69455e3c-131.jpg', 1, 54);
 
 -- --------------------------------------------------------
 
@@ -173,21 +121,6 @@ CREATE TABLE `users` (
   `passwd` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `email`, `passwd`) VALUES
-(11, 'Admin', 'Admin', 'admin@mail.com', '$2y$10$//FAoi3uPEbbL5exO/f67OQ3bjOu0aFAQuU4qQKKaJ2MwrJ2axnb2'),
-(12, 'Root', 'Crack', 'crack@mail.com', '$2y$10$J7fztsiCNYRiYTqmkHIvhe5Nga4ifyz.JbRzzoe1DQGjMfNIOaDLi'),
-(13, 'Caesar', 'Kalikunde', 'caesar@mail.com', '$2y$10$hh2V54ueAhIpMZQfMOo/yOA98i47PY/8aH/rLyKw5LAw9BWYxrhUq'),
-(14, 'John', 'Doe', 'john2648@mail.com', '$2y$10$//FAoi3uPEbbL5exO/f67OQ3bjOu0aFAQuU4qQKKaJ2MwrJ2axnb2'),
-(15, 'Jane', 'Smith', 'jane9863@mail.com', '$2y$10$//FAoi3uPEbbL5exO/f67OQ3bjOu0aFAQuU4qQKKaJ2MwrJ2axnb2'),
-(16, 'Michael', 'Brown', 'michael4373@mail.com', '$2y$10$//FAoi3uPEbbL5exO/f67OQ3bjOu0aFAQuU4qQKKaJ2MwrJ2axnb2'),
-(17, 'Emily', 'Davis', 'emily9274@mail.com', '$2y$10$//FAoi3uPEbbL5exO/f67OQ3bjOu0aFAQuU4qQKKaJ2MwrJ2axnb2'),
-(18, 'Chris', 'Wilson', 'chris5251@mail.com', '$2y$10$//FAoi3uPEbbL5exO/f67OQ3bjOu0aFAQuU4qQKKaJ2MwrJ2axnb2'),
-(19, 'Root', 'Code', 'code@mail.com', '$2y$10$WQ4KV1lgforZMpbhQb6souIjnkcPL0QCVyeckNjDW3iTmYEZy2FeO');
-
 -- --------------------------------------------------------
 
 --
@@ -198,16 +131,6 @@ CREATE TABLE `user_roles` (
   `user_id` int(11) NOT NULL,
   `role_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `user_roles`
---
-
-INSERT INTO `user_roles` (`user_id`, `role_id`) VALUES
-(11, 2),
-(12, 2),
-(13, 1),
-(19, 1);
 
 --
 -- Indexes for dumped tables
@@ -277,25 +200,25 @@ ALTER TABLE `user_roles`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `product_images`
 --
 ALTER TABLE `product_images`
-  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=186;
+  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `roles`
@@ -307,7 +230,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
