178329d24cb7972cd0acdbea6a77080d02ac8a71
Bester Gondwe <143722159+Bester-Gondwe@users.noreply.github.com>
2025-05-04 20:35:48 +0200

    MINOR changes

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ session_start();
 
   <div class="bg-gold-500 text-white h-screen flex items-center justify-center">
     <div class="text-center p-8 max-w-lg mx-auto">
-      <h1 class="text-4xl font-bold mb-4">Welcome to the world of e-digital marketing</h1>
+      <h1 class="text-4xl font-bold mb-4">WelcomE to the world of e-digital marketing</h1>
       <p class="text-lg mb-6">Discover amazing products and services designed for you. Feel free to explore!!!</p>
       <a href="category.php" class="bg-yellow-500 text-black px-6 py-3 text-xl rounded-lg hover:bg-yellow-400 transition duration-300">Shop Now</a>
     </div>
